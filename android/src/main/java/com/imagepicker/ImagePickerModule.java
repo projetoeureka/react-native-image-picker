bf29188f72c38a3a15c1487e63af2c0654932c8b
felipeViana <felipe.viana@geekie.com.br>
2018-03-14 21:07:47 -0300

    Avoids calling invokeCancel without callback

--- android/src/main/java/com/imagepicker/ImagePickerModule.java
+++ android/src/main/java/com/imagepicker/ImagePickerModule.java
@@ -198,7 +198,10 @@ public class ImagePickerModule extends ReactContextBaseJavaModule
 
   public void doOnCancel()
   {
-    responseHelper.invokeCancel(callback);
+    if (callback != null) {
+      responseHelper.invokeCancel(callback);
+      callback = null;
+    }
   }
 
   public void launchCamera()
